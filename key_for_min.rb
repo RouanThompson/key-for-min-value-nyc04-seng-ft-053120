61b6bb69f0718d63b47ca91d78a6d3be183e2cf4
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-12 03:05:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   last_key = name_hash[0]
-  last_value = name_hash[0]
+  last_value = name_hash[0][0]
   name_hash.each {|key, value|
     if value < last_value
       last_value = value
